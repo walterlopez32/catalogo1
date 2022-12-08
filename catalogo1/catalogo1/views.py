ca4cff817ef7d1d0d3f70e1e563117e2255572b9
walter <walterluislopez80@gmail.com>
2022-12-08 00:05:56 -0300

    Logramos cargar varios productos

--- catalogo1/catalogo1/views.py
+++ catalogo1/catalogo1/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from productos.models import productos
 template_name = "index.html"
+
+"""p = productos.objects.create(
+nombre ="pantalo",
+precio= 2000,
+descripcion = "vintage","""""
+
+productos = productos.objects.all()
 contexto = {
-    
+    'productos': productos
 }
 def inicio(requets):
     return render(requets, template_name,contexto)
+
--- catalogo1/productos/models.py
+++ catalogo1/productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class productos(models.Model):
+    nombre = models.CharField(max_length=255)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    descripcion = models.TextField()
+
+
+    def __str__(self):
+        return self.nombre
